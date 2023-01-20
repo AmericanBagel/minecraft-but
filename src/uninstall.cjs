553ac26bdee18e9ef980bb515c93b8eb2fe4358d
AmericanBagel <92078772+AmericanBagel@users.noreply.github.com>
2023-01-19 21:43:22 -0800

    Clean up uninstall script

--- src/uninstall.cjs
+++ src/uninstall.cjs
@@ -1,55 +1,38 @@
 const fs = require('fs');
-const pathjs = require('path');
+const path = require('filepath');
+const dir = path.join(__dirname, '../data/abchc/functions/modifiers');
 
-const dir = pathjs.join(__dirname, '../data/abchc/functions/modifiers');
-
-/*
-    - For directory - scanDir()
-        - Get list of all files
-            - scanFile()
-                - fs.readFileSync()
-                - Split into lines
-                - lines.forEach(line =>
-                    - line.match((?<!scoreboard objectives add ).*(?<= .*)) to get match (console.log to see structure of result first)
-                    - Add line to list
-                - Return list
-        - Get list of all folders
-            - scanDir(folder)
-*/
-
-function getDirsInDir(path) {
-	return fs.readdirSync(path, { withFileTypes: true }).reduce((a, c) => {
+function getDirsInDir(filepath) {
+	return fs.readdirSync(filepath, { withFileTypes: true }).reduce((a, c) => {
 		c.isDirectory() && a.push(c.name);
 		return a;
 	}, []);
 }
 
-function getFilesInDir(path) {
-    return fs.readdirSync(path, { withFileTypes: true }).reduce((a, c) => {
+function getFilesInDir(filepath) {
+    return fs.readdirSync(filepath, { withFileTypes: true }).reduce((a, c) => {
 		!c.isDirectory() && a.push(c.name);
 		return a;
 	}, []);
 }
 
-function scanDir(path) {
-	const directories = getDirsInDir(path);
-    const files = getFilesInDir(path);
+function scanDir(filepath) {
+	const directories = getDirsInDir(filepath);
+    const files = getFilesInDir(filepath);
 
     directories.forEach(directory => {
-        console.log(pathjs.join(path, directory));
-        scanDir(pathjs.join(path, directory));
+        scanDir(path.join(filepath, directory));
     })
 
     files.forEach(file => {
-        console.log(pathjs.join(path, file));
-        scanFile(pathjs.join(path, file));
+        scanFile(path.join(filepath, file));
     })
 }
 
-function scanFile(path) {
+function scanFile(filepath) {
     // Only proceed if file is mcfunction
-    if (pathjs.basename(path).match(/\.mcfunction$/) !== null) {
-        const text = fs.readFileSync(path, "utf-8")
+    if (path.basename(filepath).match(/\.mcfunction$/) !== null) {
+        const text = fs.readFileSync(filepath, "utf-8")
         const arr = text.split("\n");
 
         const objectives = [];
@@ -66,11 +49,11 @@ function scanFile(path) {
                 uninstallMCF += 'scoreboard objectives remove ' + e + '\n'
             })
 
-            fs.writeFileSync(pathjs.join(pathjs.dirname(path), "uninstall.mcfunction"), uninstallMCF)
+            console.log("Wrote " + path.join(path.dirname(filepath), "uninstall.mcfunction"))
+            fs.writeFileSync(path.join(path.dirname(filepath), "uninstall.mcfunction"), uninstallMCF)
         }
     }
 }
 
 console.log(`Reading directory ${dir}`);
-//scanDir(dir);
-scanDir(pathjs.join(dir));
+scanDir(path.join(dir));
